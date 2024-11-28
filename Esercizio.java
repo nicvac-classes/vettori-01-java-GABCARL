7f3cbe00501a8f840f5b04138fd5738aa4ad0c3d
GABCARL <gabriele.carlone@marconibari.edu.it>
2024-11-28 15:43:31 +0000

    consegnato

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,60 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String v[];
+        v=new String [5];
+        v[0]="matt";
+        v[1]="paky";
+        v[2]="amy";
+        v[3]= "bono";
+        v[4]=" mario";
+        int i;
+        i=0;
+        while(i<5){
+            System.out.println("scrivi v[i]");
+            i++;
+        }
+        String w[];
+        w= new String[5];
+        while(i<5){
+            System.out.println("scrivi w[i]");
+            i++;
+        }
+        i=0;
+        for (i=0; i<5; i++){
+            System.out.println("inserire il nome del canatnte");
+            w[i]= scanner.nextline();
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
-    }
-}
-
+        }
+        int x[];
+        x= new int[10];
+        while(i<10){
+            System.out.println("x[i]");
+            System.out.println("il contenuto degli indici pari sono");
+            System.out.println("x[2]");
+            System.out.println("x[4]");
+            System.out.println("il contenuto degli indici dispari sono");
+            System.out.println("x[1]");
+            System.out.println("x[3]");
+            System.out.println("x[5]");
+        }
+        i=0;
+        for( i=0; i<10; i++){
+            x[i]= scanner.nextInt();
+        }
+        int y[];
+        y= new Int [10];
+        while(i<10){
+            System.out.println(" y[i]");
+            i++;
+        }
+        int resto;
+        int i;
+        resto=0;
+        i=0;
+        for(i=0;i<10;i++){
+            y[i]=resto/2;
+        }
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
